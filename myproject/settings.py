63484532e725fe6011cab0898ba71d4c6b78647b
raoabrar8 <fa23-rcs-014@cuilahore.edu.pk>
2024-08-26 09:14:16 -0700

    added the whitomes

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,3 +147,6 @@ if 'DYNO' in os.environ:  # This check determines if the app is running on Herok
 
     # Ensure WhiteNoise finds your static files
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
