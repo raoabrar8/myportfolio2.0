aa8fde181d3d68bc24b6223a1bd6e574645de0da
raoabrar8 <fa23-rcs-014@cuilahore.edu.pk>
2024-08-26 19:41:01 -0700

    added the css and js inthe internal base.html

--- static/js/script.js
+++ static/js/script.js
@@ -75,24 +75,6 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-
-const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
